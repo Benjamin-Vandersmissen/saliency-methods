f0f016254262fcf909e986fb102b8180d1437d9c
Benjamin-Vandersmissen <benjamin.vandersmissen@student.uantwerpen.be>
2021-12-03 11:56:54 +0100

    Added a lot of comments to explain the LRP autograd mechanism in the rule.py file. Added a convenience function to easily assign different rules to different layer types. Cleanup of previous LRP class and Rule classes

--- saliency_methods/lrp.py
+++ saliency_methods/lrp.py
@@ -1,15 +1,12 @@
-import copy
 import types
 
 import numpy as np
 import torch
-import torch.nn.functional as F
+import torch.nn as nn
 
 from .base import SaliencyMethod
 from .utils import extract_layers
 from .rule import *
-
-__all__ = ['LRP']
 
 # TODO: https://pytorch.org/tutorials/beginner/examples_autograd/two_layer_net_custom_function.html
 # TODO: https://github.com/dmitrysarov/LRP_decomposition/tree/72031a5252aafb7f7e51abe28bba40dba0fce9e7
@@ -17,25 +14,13 @@ __all__ = ['LRP']
 
 
 class LRPBackward(SaliencyMethod):
+    """
+    On Pixel-Wise Explanations for Non-Linear Classifier Decision by Layer-wise Relevance Propagation (Bach et al. 2015)
+    """
     def __init__(self, net: nn.Module, **kwargs):
         super().__init__(net, **kwargs)
 
-        first_layer = True
-        for module in self.net.modules():
-            if len(list(module.modules())) == 1:
-                setattr(module, 'forward_orig', module.forward)
-                if not (isinstance(module, nn.ReLU) or isinstance(module, nn.BatchNorm2d)):
-                    if first_layer:
-                        setattr(module, 'forward', types.MethodType(getattr(LRPZbRule, 'forward'), module))
-                        first_layer = False
-                    else:
-                        if isinstance(module, nn.AdaptiveAvgPool2d):
-                            setattr(module, 'forward', types.MethodType(getattr(LRPZeroRule, 'forward'), module))
-                        else:
-                            setattr(module, 'forward', types.MethodType(getattr(LRPAlphaBetaRule, 'forward'), module))
-
-                else:
-                    setattr(module, 'forward', types.MethodType(getattr(LRPIdentityRule, 'forward'), module))
+        self.assign_rules()
 
     def calculate_map(self, in_values: torch.tensor, labels: torch.Tensor, **kwargs) -> np.ndarray:
         in_values.to(self.device).requires_grad_(True)
@@ -46,94 +31,28 @@ class LRPBackward(SaliencyMethod):
         grad = torch.autograd.grad(out_values, in_values, grad_out)[0]
         return grad.detach().numpy()
 
+    def assign_rules(self):
 
-class LRP(SaliencyMethod):
-    """
-    On Pixel-Wise Explanations for Non-Linear Classifier Decision by Layer-wise Relevance Propagation (Bach et al. 2015)
-    """
+        identity_layers = [nn.ReLU, nn.BatchNorm2d, nn.Dropout]
+        zero_layers = [nn.AdaptiveAvgPool2d, nn.AvgPool2d, nn.MaxPool2d]
+        alpha_beta_layers = [nn.Conv2d, nn.Linear]
 
-    def __init__(self, net: nn.Module, rule: Rule = CompositeRule({0:ZbRule(), 1:LRPeps()}), **kwargs):
-        """
-        Initialize a new LRP Saliency Method object.
-        :param net: The neural network to use.
-        :param rule: The rule(s) to apply during the propagation process.
-        :param kwargs: Other arguments.
-        """
-        super().__init__(net, **kwargs)
+        layer_map = {layer: LRPIdentityRule for layer in identity_layers}
+        layer_map.update({layer: LRPZeroRule for layer in zero_layers})
+        layer_map.update({layer: LRPAlphaBetaRule for layer in alpha_beta_layers})
 
-        self.layers = None
-        self.rule = rule
+        initial_layer = True
+        for layer in self.net.modules():
+            if len(list(layer.children())) == 0:
+                if layer.__class__ not in layer_map:
+                    raise Exception("There is no rule associated with this layer! (%s)" % layer.__class__)
 
-    def _process_layers(self, shape: tuple):
-        """ Remove the flatten layer and replace the Linear layers with equivalent Conv2D layers.
-
-        Parameters
-        ----------
-
-        shape : 4D-tuple of shape (batch, channel, width, height)
-            The shape of the input the neural network expects.
-
-        """
-        dummy_value = torch.zeros(shape).to(self.device)
-
-        new_layers = []
-        for _, layer in self.layers:
-
-            if isinstance(layer, nn.Linear):  # convert linear layers to corresponding convolutional layers
-                new_layer = None
-                if dummy_value.shape[2] != 1:  # This layer replaces the flatten layer and the first linear layer
-                    m, n = dummy_value.shape[1], layer.weight.shape[0]
-                    new_layer = nn.Conv2d(m, n, (dummy_value.shape[2], dummy_value.shape[3]))  # Flatten into 1x1 output
-                    new_layer.weight = nn.Parameter(layer.weight.reshape(n, m, dummy_value.shape[2], dummy_value.shape[3]))
+                setattr(layer, 'forward_orig', layer.forward)
+                if initial_layer:
+                    setattr(layer, 'forward', types.MethodType(getattr(LRPZbRule, 'forward'), layer))
+                    initial_layer = False
                 else:
-                    m, n = layer.weight.shape[1], layer.weight.shape[0]
-                    new_layer = nn.Conv2d(m, n, 1)
-                    new_layer.weight = nn.Parameter(layer.weight.reshape(n, m, 1, 1))
-                new_layer.bias = nn.Parameter(layer.bias)
-                new_layers.append(new_layer.to(self.device))
-                dummy_value = new_layer.forward(dummy_value)
-
-            elif not isinstance(layer, nn.Flatten):
-                dummy_value = layer.forward(dummy_value)
-
-                if isinstance(layer, nn.MaxPool2d):
-                    new_layers.append(nn.AvgPool2d(layer.kernel_size, layer.stride))
-                else:
-                    new_layers.append(layer)
-
-        self.layers = new_layers
-
-    def calculate_map(self, in_values: torch.tensor, labels: torch.Tensor, **kwargs) -> np.ndarray:
-        """ Calculates the Layer-wise Relevance Propagation of the input w.r.t. the desired label.
-
-        Parameters
-        ----------
-
-        in_values : 4D-tensor of shape (batch, channel, width, height)
-            A batch of images we want to generate saliency maps for.
-
-        labels : 1D-tensor containing *batch* elements.
-            The labels for the images we want to explain for.
-
-        Returns
-        -------
-
-        4D-numpy.ndarray
-            A batch of saliency maps for the images and labels provided.
-
-        """
-        in_values.to(self.device)
-        labels = labels.reshape(labels.shape[0], 1)
-
-        if self.layers is None:
-            self.layers = extract_layers(self.net, in_values.shape)
-            self._process_layers(in_values.shape)
-
-        activations = [in_values] + [None] * len(self.layers)
-        for i in range(len(self.layers)):
-            activations[i + 1] = self.layers[i].forward(activations[i])
-
-        return self.rule(self.layers, activations, labels)
+                    setattr(layer, 'forward', types.MethodType(getattr(layer_map[layer.__class__], 'forward'), layer))
 
 
 class MarginalWinningProbability(SaliencyMethod):
--- saliency_methods/rule.py
+++ saliency_methods/rule.py
@@ -1,9 +1,7 @@
 import copy
-from abc import ABC, abstractmethod
 from typing import Any
 
 from torch import nn
-from collections.abc import Callable
 from copy import deepcopy
 from functools import partial
 
@@ -11,10 +9,35 @@ from .utils import EPSILON
 
 import torch
 
-# First for each module in the network that is supported (make list of supported modules) and check with in keyword:
-# Rename the forward function to forward_orig and add the overridden forward module from LRPRule
-# During the new forward pass, we call the original forward pass
+__all__ = ['LRPfunc', 'LRPRule', 'LRPZeroRule', 'LRPIdentityRule', 'LRPEpsilonRule', 'LRPgammaRule', 'LRPAlphaBetaRule',
+           'LRPZbRule']
 
+# The following classes implement different Rules for Layerwise-Relevance Propagation.
+# The LRP process is implemented efficiently using the pytorch autograd mechanism. To accomplish this, we introduced a
+# new class LRPfunc, which is a subclass of an autograd function and enables a module to backpropagate relevance.
+
+# Each rule has a forward method associated and to implement a rule for an instance of a module, the module::forward()
+# function needs to be renamed to module::forward_orig(),
+# while a new bound method Rule::forward() is bound to module::forward().
+# This can be done using '''setattr(module, 'forward_orig', module.forward)''' and
+# '''setattr(module, 'forward', types.MethodType(getattr(Rule, 'forward'), module))
+#
+# Rule::forward() calls the overridden LRPfunc::forward() and provides it with the original input,
+# module and the chosen relevance function.
+# LRPfunc::forward() then calls module::forward_orig() on the input and saves the module, input and relevance function
+# During Rule::backward, LRPfunc::backward is automatically called due to the autograd mechanism.
+# LRPfunc::backward invokes the relevance function with the needed arguments and backpropagates the results.
+
+
+#
+# The following rules are implemented:
+# - LRPRule : returns the gradient
+# - LRPZeroRule : use LRP-0
+# - LRPIdentityRule : returns the relevance of the previous layer
+# - LRPEpsilonRule : use LRP-\eps (denoise the relevance from contradictory / weak contributions)
+# - LRPGammaRule : use LRP-\gamma (add more weight to positive contributions)
+# - LRPAlphaBetaRule : use LRP-\alpha,\beta (adds weights for positive and negative contributions
+# - LRPZbRule : implements the Zb rule (specific rule for the input layer.)
 
 class LRPRule(object):
 
@@ -22,8 +45,44 @@ class LRPRule(object):
     def relevance_func(inp, relevance, module):
         return inp
 
+
+    @staticmethod
+    def _modify_layer(layer: nn.Module, func) -> nn.Module:
+        """ Modify a layer by applying a function on the weights and biases.
+
+        Parameters
+        ----------
+
+        layer : torch.nn.module
+            The layer to modify.
+
+        func : callable
+            The function that will modify the weights and biases.
+
+        Returns
+        -------
+
+        new_layer : torch.nn.module
+            The layer with modified weights and biases.
+
+        """
+        new_layer = deepcopy(layer)
+
+        try:
+            new_layer.weight = nn.Parameter(func(layer.weight))
+        except AttributeError:
+            pass
+
+        # try:
+        #     if layer.bias is not None:
+        #         new_layer.bias = nn.Parameter(func(layer.bias))
+        # except AttributeError:
+        #     pass
+
+        return new_layer
+
     def forward(self, input, relevance_func=relevance_func.__func__):
-        return LRP_func.apply(self, input, relevance_func)
+        return LRPfunc.apply(self, input, relevance_func)
 
 
 class LRPIdentityRule(LRPRule):
@@ -43,7 +102,7 @@ class LRPZeroRule(LRPRule):
         inp.requires_grad_(True)
         if inp.grad is not None:
             inp.grad.zero_()
-        module = Rule._modify_layer(module, rho)
+        module = LRPRule._modify_layer(module, rho)
         with torch.enable_grad():
             Z = incr(module.forward_orig(inp))
             Z = Z + (Z == 0) * EPSILON
@@ -66,9 +125,9 @@ class LRPAlphaBetaRule(LRPRule):
             pos_incr = lambda z: torch.clamp(z, min=0)
             neg_incr = lambda z: torch.clamp(z, max=0)
 
-            pos_model = Rule._modify_layer(module, pos_incr)
+            pos_model = LRPRule._modify_layer(module, pos_incr)
             pos_model.bias = None
-            neg_model = Rule._modify_layer(module, neg_incr)
+            neg_model = LRPRule._modify_layer(module, neg_incr)
             neg_model.bias = None
 
             zpos_pos = pos_model.forward_orig(pos_inp)
@@ -115,8 +174,8 @@ class LRPZbRule(LRPRule):
             ub.retain_grad()
 
             z = module.forward_orig(inp)
-            z -= Rule._modify_layer(module, lambda p: p.clamp(min=0)).forward_orig(lb)  # - lb * w+
-            z -= Rule._modify_layer(module, lambda p: p.clamp(max=0)).forward_orig(ub)  # - ub * w-
+            z -= LRPRule._modify_layer(module, lambda p: p.clamp(min=0)).forward_orig(lb)  # - lb * w+
+            z -= LRPRule._modify_layer(module, lambda p: p.clamp(max=0)).forward_orig(ub)  # - ub * w-
 
             z = z + (z == 0) * EPSILON  # for numerical stability
 
@@ -149,7 +208,7 @@ class LRPgammaRule(LRPZeroRule):
 #         return LRPRule.forward(self, input, LRPbatchnormRule.relevance_func)
 
 
-class LRP_func(torch.autograd.Function):
+class LRPfunc(torch.autograd.Function):
 
     @staticmethod
     def forward(ctx: Any, module, inp, relevance_func) -> Any:
@@ -162,163 +221,3 @@ class LRP_func(torch.autograd.Function):
     def backward(ctx: Any, relevance) -> Any:
         new_relevance = ctx.relevance_func(ctx.inp, relevance, ctx.module)
         return None, new_relevance, None
-
-
-class Rule(ABC):
-
-    def __init__(self):
-        pass
-
-    @abstractmethod
-    def relevance(self, layer, activation, prev_relevance, layer_idx):
-        raise NotImplementedError("A Subclass of Rule needs to implement this function")
-
-    def __call__(self, layers, activations, labels):
-
-        shape = activations[-1].shape[0:2]
-        # Keep only the relevant score, mask the rest
-        values = torch.gather(activations[-1].reshape(shape), 1, labels)
-
-        relevances = [torch.zeros(shape).scatter_(1, labels, values).reshape(*shape, 1, 1)]
-
-        #  ! use .data to make a leaf node so we can use autograd
-        for j in range(len(layers) - 1, -1, -1):
-            activation = activations[j].data.requires_grad_(True)
-            layer = layers[j]
-
-            if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.MaxPool2d) or isinstance(layer, nn.AvgPool2d):
-                relevances.append(self.relevance(layer, activation, relevances[-1], j))
-
-            else:
-                relevances.append(relevances[-1])
-
-        return relevances[-1].detach().cpu().numpy()
-
-    @staticmethod
-    def _modify_layer(layer: nn.Module, func) -> nn.Module:
-        """ Modify a layer by applying a function on the weights and biases.
-
-        Parameters
-        ----------
-
-        layer : torch.nn.module
-            The layer to modify.
-
-        func : callable
-            The function that will modify the weights and biases.
-
-        Returns
-        -------
-
-        new_layer : torch.nn.module
-            The layer with modified weights and biases.
-
-        """
-        new_layer = deepcopy(layer)
-
-        try:
-            new_layer.weight = nn.Parameter(func(layer.weight))
-        except AttributeError:
-            pass
-
-        # try:
-        #     if layer.bias is not None:
-        #         new_layer.bias = nn.Parameter(func(layer.bias))
-        # except AttributeError:
-        #     pass
-
-        return new_layer
-
-
-class LRP0(Rule):
-    def __init__(self):
-        super().__init__()
-        self.rho = lambda p: p
-        self.incr = lambda z: z
-
-    def relevance(self, layer, activation, prev_relevance, layer_idx):
-        z = self.incr(self._modify_layer(layer, self.rho).forward(activation))  # step 1
-        z[z == 0] = EPSILON  # for numeric stability
-        s = (prev_relevance / z).data  # step 2
-        (z * s).sum().backward()  # step 3
-        c = activation.grad
-        return (activation * c).data
-
-
-class LRPeps(LRP0):
-    def __init__(self, eps="auto"):
-        super().__init__()
-        if eps == "auto":
-            self.incr = lambda z: z + 0.25 * ((z ** 2).mean() ** .5).data
-        else:
-            self.incr = lambda z: z + eps
-
-
-class LRPgamma(LRP0):
-    def __init__(self, gamma=0.25):
-        super().__init__()
-        self.rho = lambda p: p + gamma * p.clamp(min=0)
-
-
-class ZbRule(Rule):
-    def __init__(self, mean=torch.zeros((1, 3, 1, 1)), std=torch.ones((1, 3, 1, 1))):
-        super().__init__()
-        self.mean = mean
-        self.std = std
-
-    def relevance(self, layer, activation, prev_relevance, layer_idx):
-        device = activation.device
-        self.mean = self.mean.to(device)
-        self.std = self.std.to(device)
-
-        lb = (activation.data * 0 - self.std).requires_grad_(True)  # lower bound on the activation
-        ub = (activation.data * 0 + self.std).requires_grad_(True)  # upper bound on the activation
-
-        z = layer.forward(activation)
-        z -= self._modify_layer(layer, lambda p: p.clamp(min=0)).forward(lb)  # - lb * w+
-        z -= self._modify_layer(layer, lambda p: p.clamp(max=0)).forward(ub)  # - ub * w-
-
-        z[z == 0] = EPSILON  # for numerical stability
-
-        s = (prev_relevance / z).data  # step 2
-        (z * s).sum().backward()  # step 3
-        c, cp, cm = activation.grad, lb.grad, ub.grad
-        return activation * c + lb * cp + ub * cm  # step 4
-
-
-# combine multiple rules.
-class CompositeRule(Rule):
-
-    def __init__(self, rules=None, pixel=None, feature=None):
-        super().__init__()
-
-        self.rules = []
-
-        if isinstance(rules, dict):
-            keys = sorted([key for key in rules.keys()])
-            self.rules = []
-            prev_idx = 0
-            for key in keys:
-                rule = rules[key]
-                if len(self.rules) == 0:
-                    self.rules += [rule]
-                else:
-                    self.rules += (key-prev_idx-1)*[self.rules[-1]]
-                    self.rules += [rule]
-                prev_idx = key
-
-            #TODO: assert on structure of rules : only numeric keys, or list of features (should match).
-        elif rules is None and pixel is not None and feature is not None:
-            self.rules = {"pixel": pixel, "feature": feature}
-
-        pass
-
-    def relevance(self, layer, activation, prev_relevance, layer_idx):
-        if "pixel" in self.rules and layer_idx == 0:
-            return self.rules["pixel"].relevance(layer, activation, prev_relevance, layer_idx)
-
-        elif "feature" in self.rules and layer_idx != 0:
-            return self.rules["feature"].relevance(layer, activation, prev_relevance, layer_idx)
-
-        else:
-            return self.rules[min(layer_idx, len(self.rules)-1)].relevance(layer, activation, prev_relevance, layer_idx) # Failsafe for if there are more layers than specified
