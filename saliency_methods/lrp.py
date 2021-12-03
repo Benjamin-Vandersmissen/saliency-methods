d7f861fd6d763620a04a08b895cc1cfbd8b9d57a
Benjamin-Vandersmissen <benjamin.vandersmissen@student.uantwerpen.be>
2021-12-03 12:08:30 +0100

    Additional cleanup of the lrp.py file.

--- saliency_methods/lrp.py
+++ saliency_methods/lrp.py
@@ -5,24 +5,48 @@ import torch
 import torch.nn as nn
 
 from .base import SaliencyMethod
-from .utils import extract_layers
 from .rule import *
 
-# TODO: https://pytorch.org/tutorials/beginner/examples_autograd/two_layer_net_custom_function.html
-# TODO: https://github.com/dmitrysarov/LRP_decomposition/tree/72031a5252aafb7f7e51abe28bba40dba0fce9e7
-# Implement rules as custom autograd functions, with forward and backward.
+__all__ = ['LRP']
 
 
-class LRPBackward(SaliencyMethod):
+class LRP(SaliencyMethod):
     """
     On Pixel-Wise Explanations for Non-Linear Classifier Decision by Layer-wise Relevance Propagation (Bach et al. 2015)
+    Special thanks to:
+    https://github.com/wjNam/Relative_Attributing_Propagation for an LRP implementation working with Resnet to compare against
+    https://github.com/dmitrysarov/LRP_decomposition for a working LRP implementation using autograd.
     """
     def __init__(self, net: nn.Module, **kwargs):
+        """
+        Initialize a new LRP Saliency Method object.
+        :param net: The neural network to use.
+        :param kwargs: Other arguments.
+        """
         super().__init__(net, **kwargs)
 
         self.assign_rules()
 
     def calculate_map(self, in_values: torch.tensor, labels: torch.Tensor, **kwargs) -> np.ndarray:
+        """ Calculates the LRP map of the input w.r.t. the desired labels.
+
+        Parameters
+        ----------
+
+        in_values : 4D-tensor of shape (batch, channel, width, height)
+            A batch of images we want to generate saliency maps for.
+
+        labels : 1D-tensor containing *batch* elements.
+            The labels for the images we want to explain for.
+
+        Returns
+        -------
+
+        4D-numpy.ndarray
+            A batch of saliency maps for the images and labels provided.
+
+        """
+
         in_values.to(self.device).requires_grad_(True)
         labels = labels.reshape(labels.shape[0], 1)
         out_values = self.net(in_values)
@@ -32,7 +56,12 @@ class LRPBackward(SaliencyMethod):
         return grad.detach().numpy()
 
     def assign_rules(self):
+        """ Assign LRP rules to each layer in the neural network
 
+        Throws
+        ------
+        Exception if a layer has no associated rule.
+        """
         identity_layers = [nn.ReLU, nn.BatchNorm2d, nn.Dropout]
         zero_layers = [nn.AdaptiveAvgPool2d, nn.AvgPool2d, nn.MaxPool2d]
         alpha_beta_layers = [nn.Conv2d, nn.Linear]
