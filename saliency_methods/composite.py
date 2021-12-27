Fixed the implementation of GuidedBackPropagation (previously, it clipped only when the gradient was < 0, like DeConvNets, but not when the input was < 0.) Also removed the hooks after GuidedBP was done in the Guided composite class. This prevents incorrect gradients in the explanation method that is guided. Fixed a small issue with a .squeeze() in the Smooth composite class.

saliency_methods/composite.py
@@ -71,7 +71,7 @@ class Smooth(CompositeSaliencyMethod):
         for i in range(self.smooth_rate):
             noisy_input = self.noise_func(in_values.clone())
             saliency_maps[i, :] = super().calculate_map(noisy_input, labels, **kwargs)
-        saliency = saliency_maps.mean(axis=0).squeeze()
+        saliency = saliency_maps.mean(axis=0)
         return saliency
 
 
@@ -110,4 +110,7 @@ class Guided(CompositeSaliencyMethod):
 
         """
         guide = self.guidedBP.calculate_map(in_values, labels, **kwargs)
+        for hook in self.guidedBP.hooks:
+            hook.remove()
+        self.guidedBP.hooks = []
         return guide * super().calculate_map(in_values, labels, **kwargs)

saliency_methods/gradient.py
@@ -256,15 +256,26 @@ class GuidedBackProp(Gradient):
 
     def hook_grad(self):
         """
-        Add backward hooks to the ReLU modules, to clip their gradients to positive values.
+        Add forward and backward hooks to the ReLU modules, to clip their gradients to positive values.
         :return: /
         """
-        def hook(module, in_grad, out_grad):
-            return tuple(F.relu(out_grad[0]), )
+        def back_hook(module, in_grad, out_grad):
+            outp = module.outp.pop()
+            if len(module.outp) == 0:
+                del module.outp
+            out = F.relu((outp != 0) * out_grad[0])
+            return out,
+
+        def forward_hook(module, inp, outp):
+            if hasattr(module, 'outp'):
+                module.outp.append(outp)
+            else:
+                module.outp = [outp]
 
         for module in self.net.modules():
             if isinstance(module, nn.ReLU):
-                self.hooks.append(module.register_backward_hook(hook))
+                self.hooks.append(module.register_backward_hook(back_hook))
+                self.hooks.append(module.register_forward_hook(forward_hook))
 
     def calculate_map(self, in_values: torch.tensor, labels: torch.Tensor, **kwargs) -> np.ndarray:
         """ Calculates the Guided Backpropagation of the input w.r.t. the desired label.